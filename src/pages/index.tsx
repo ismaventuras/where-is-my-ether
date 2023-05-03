f4ee7d9c53b0e42ec644bfeca88e0be36ede0932
Ismael Bautista <ismaelbautista6@gmail.com>
2023-05-03 20:11:19 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className='p-2 md:p-0 container mx-auto max-w-lg'>
       <div className='text-center mb-4'>
-        <h1 className='text-3xl font-bold flex justify-center items-center'>Where is my <Image src="/ethereum_moving.gif"  alt="ETH" width={64} height={64}/> ?</h1>
+        <h1 className='text-3xl font-bold flex justify-center items-center'>Where is my <Image src="ethereum_moving.gif"  alt="ETH" width={64} height={64}/> ?</h1>
         <h2 className='text-xl font-light'>Have been sending your ether across chains and now you don't know where it is? Paste your address and find how many ether you have on every chain using ether as native currency!</h2>
       </div>
       <Form state={state} setState={setState} />
